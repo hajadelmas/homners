b323318eddb9bdfceb8694b07f3d0e5f90c4e8e2
hajadelmas <52510008+hajadelmas@users.noreply.github.com>
2021-03-25 14:50:14 +0100

    add browser-monads for window object

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import { window } from 'browser-monads'
 import PropTypes from "prop-types"
 import { Link } from 'gatsby'
 import { StaticImage } from "gatsby-plugin-image"
