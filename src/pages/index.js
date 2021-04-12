d6e94ff6f8051bc719c2168c39842ede8565620d
hajadelmas <52510008+hajadelmas@users.noreply.github.com>
2021-04-13 00:20:03 +0200

    modif general

--- src/pages/index.js
+++ src/pages/index.js
@@ -163,10 +163,10 @@ const IndexPage = ({ data }) => {
     </div> */}
 
 
-    <p>
+    {/* <p>
       <Link to="/page-2/">Go to page 2</Link> <br />
       <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
+    </p> */}
     
   </Layout>
   )
--- src/pages/travaux.js
+++ src/pages/travaux.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Layout from '../components/layout'
-// import ReactCompareImage from 'react-compare-image'
 import { ImgComparisonSlider } from '@img-comparison-slider/react';
 
 
-import Img1 from '../images/travaux/compare1.jpg'
-import Img2 from '../images/travaux/compare2.jpg'
+import Img1 from '../images/travaux/porte_avant (1).jpeg'
+import Img2 from '../images/travaux/porte_apres (1).jpeg'
+import Img3 from '../images/travaux/herbe_avant (1).jpeg'
+import Img4 from '../images/travaux/herbe_apres (1).jpeg'
 
 
 
@@ -18,15 +19,28 @@ const travaux = () => {
                 <h1>Travaux / Home Stagging</h1>
                 <p>Confiez-nous l'embellissement de votre bien : jardin, mur, rénovations, petits travaux, embellissement intérieur ou extérieur, et avec un minimum d'investissement, obtenez une valorisation incomparable.</p>
                 <p><strong>Nous prenons ces travaux à notre CHARGE !</strong></p>
-                
             </div>
 
-            <div className='compare_container'>
-               {/* <ReactCompareImage leftImage={Img1} rightImage={Img2} />  */}
-               <ImgComparisonSlider>
-                    <img slot="before" src={Img1} alt=''/>
-                    <img slot="after" src={Img2} alt=''/>
-                </ImgComparisonSlider>
+            <div className='container_travaux'>
+                <h1>Exemple travaux</h1>
+
+                <div className='compare_container'>
+                    <h1>Avant</h1>
+                    <ImgComparisonSlider>
+                        <img slot="before" src={Img1} alt=''/>
+                        <img slot="after" src={Img2} alt=''/>
+                    </ImgComparisonSlider>
+                    <h1>Après</h1>
+                </div>
+                <div className='compare_container'>
+                    <h1>Avant</h1>
+                    <ImgComparisonSlider>
+                        <img slot="before" src={Img4} alt=''/>
+                        <img slot="after" src={Img3} alt=''/>
+                    </ImgComparisonSlider>
+                    <h1>Après</h1>
+                </div>
+               
             </div>
 
             
