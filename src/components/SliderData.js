b346429d4fc0261604bd5e3e17b041cc3fbc3e44
hajadelmas <52510008+hajadelmas@users.noreply.github.com>
2021-08-09 22:51:01 +0200

    homners img

--- src/components/SliderData.js
+++ src/components/SliderData.js
@@ -1,5 +1,5 @@
 import ImageOne from '../images/house4.jpg'
-// import ImageTwo from '../images/house-2.jpg'
+import ImageTwo from '../images/homners2.jpg'
 import ImageThree from '../images/house-3.jpg'
 
 
@@ -13,14 +13,14 @@ export const SliderData = [
         image: ImageThree,
         alt: 'Homners'
     },
-    // {
-    //     title: 'HOMNERS',
-    //     desc: 'L\'immobilier à prix fixe',
-    //     path: '/contact-us',
-    //     label: 'Nous contacter',
-    //     image: ImageTwo,
-    //     alt: 'House'
-    // },
+    {
+        title: 'HOMNERS',
+        desc: 'L\'immobilier à prix fixe',
+        path: '/contact-us',
+        label: 'Nous contacter',
+        image: ImageTwo,
+        alt: 'House'
+    },
     {
         title: 'HOMNERS',
         desc: 'L\'immobilier à prix fixe',
